938c9fd472f0b42127cf80d4952997780929d50a
rorocodes75 <rozealotaibi@email.com>
2024-10-20 21:58:46 +0300

    update

--- search_doctor.php
+++ search_doctor.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "hospital_db.sql";
+$dbname = "hospital_DB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 

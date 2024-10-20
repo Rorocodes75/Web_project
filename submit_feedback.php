d93347ee27356709454d45dd90b989dd5ab9f90c
rorocodes75 <rozealotaibi@email.com>
2024-10-20 21:24:13 +0300

    test

--- submit_feedback.php
+++ submit_feedback.php
@@ -1,13 +1,6 @@
 <?php
 session_start();
 
-// Check if user is logged in
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header('Location: login.html');
-    exit();
-}
-
-// Database connection
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -19,7 +12,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Process feedback form submission
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $rating = $_POST['rating'];
     $comments = $conn->real_escape_string($_POST['comments']);
